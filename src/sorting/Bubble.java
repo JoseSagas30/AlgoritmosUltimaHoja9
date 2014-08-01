530a7714474b5a4597645a8ab7b2b1f2a3bbaca5
erickDM <erickdemata@gmail.com>
2014-07-31 19:41:27 -0600

    se agregaron clases de sort bubble

--- src/sorting/Bubble.java
+++ src/sorting/Bubble.java
@@ -11,5 +11,20 @@ package sorting;
  * @author Isa
  */
 public class Bubble {
-    
+    public void sort(int[] inputArr) {
+      for (int index = 1; index < inputArr.length; index++)
+      {
+        Comparable key = inputArr[index];
+        int position = index;
+
+        //  Shift larger values to the right
+        while (position > 0 && key.compareTo(inputArr[position-1]) < 0)
+        {
+           inputArr[position] = inputArr[position-1];
+           position--;
+        }
+
+        inputArr[position] = (int) key;
+      }
+    }
 }

96ca49247273bf1ecb3975bc0a19721156f384c2
con13154 <con13154@uvg.edu.gt>
2014-11-10 21:13:31 -0600

    Cambio grafos

--- src/sorting/MatrizGrafo.java
+++ src/sorting/MatrizGrafo.java
@@ -34,15 +34,14 @@ public class MatrizGrafo<V,E> implements Grafos<V,E>{
                 }
         }
     }
-   
+   //Agrega nuevo nodo
     public void agregarNodo(V contenido) {
         
         if(!vertices.contains(contenido)){
             vertices.add(contenido);
         }
     }
-
-   
+   //Agrega nueva conexion entre 2 ciudades
     public void agregarConexion(V v1, V v2, E contenido) {
         int i = vertices.indexOf(v1);
         int j = vertices.indexOf(v2);        
@@ -51,7 +50,7 @@ public class MatrizGrafo<V,E> implements Grafos<V,E>{
         nodos[i][j]=peso;
     }
 
-   
+   //Muestra matriz de adyacencia
     public String mostrarMatrizAdy(){
         
         for(int a=0;a<vertices.size();a++){
@@ -64,17 +63,15 @@ public class MatrizGrafo<V,E> implements Grafos<V,E>{
         matrizTemporal="";
         return matrizRetorno;
     }
-
- 
+ //OBtiene tamaño grafo
     public int tamanoGrafo(){
         return vertices.size();
     }
-
-  
-    public int obtenerConexion(V contenido1, V contenido2){
+//Metodo que obtiene conexion entre dos ciudades
+      public int obtenerConexion(V contenido1, V contenido2){
         return nodos[vertices.indexOf(contenido1)][vertices.indexOf(contenido2)];
     }
-  
+      //Metodo que obtiene posicion
     public int obtenerPosicion(V contenido){
         return vertices.indexOf(contenido);
     }
